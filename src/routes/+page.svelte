2ed00294ae815857ee2b3eea5037214a2aa5ca7a
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 16:28:08 -0600

    upgrade deps (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,12 @@
 	import LibraryHeader from '@fuz.dev/fuz_library/LibraryHeader.svelte';
 	import LibraryFooter from '@fuz.dev/fuz_library/LibraryFooter.svelte';
 	import {set_tomes} from '@fuz.dev/fuz_library/tome.js';
-	import {parse_package_meta} from '@fuz.dev/fuz_library/package.js';
+	import {parse_package_meta} from '@fuz.dev/fuz_library/package_meta.js';
 
 	import DialogTome from '$routes/DialogTome.svelte';
 	import {tomes} from '$routes/tomes.js';
+	import {package_json} from '$lib/package.js';
 
-	// TODO SvelteKit warns about this but we put `/static` in `/src` because of what it's saying,
-	/// maybe change to import as the first item from `packages`
-	import package_json from '../static/.well-known/package.json';
 	const pkg = parse_package_meta(package_json.homepage, package_json);
 
 	set_tomes(new Map(tomes.map((t) => [t.name, t])));
